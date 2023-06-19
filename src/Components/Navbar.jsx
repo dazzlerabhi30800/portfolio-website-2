9a2c00819f2885cc9d9be7668a3284ea259163d6
= <abhisingh30800@gmail.com>
2023-06-19 18:50:26 +0530

    fixed impact font issue and click event on close menu if outside the button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,20 @@ import { AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
   const windowWidth = Resize();
   const [showMenu, setShowMenu] = useState(false);
+  document.documentElement.addEventListener("click", checkMenu);
+
+  function checkMenu(e) {
+    e.preventDefault();
+    const path = e.composedPath();
+    if (path.some((elem) => elem.id === "navBtn")) {
+      document.documentElement.addEventListener("click", checkMenu);
+      return;
+    } else {
+      setShowMenu(false);
+      document.documentElement.removeEventListener("click", checkMenu);
+    }
+  }
+
   return (
     <nav>
       <h1>
@@ -27,7 +41,11 @@ const Navbar = () => {
         </li>
       </ul>
       {windowWidth < 700 && (
-        <button className="nav--btn" onClick={() => setShowMenu(!showMenu)}>
+        <button
+          className="nav--btn"
+          id="navBtn"
+          onClick={() => setShowMenu(!showMenu)}
+        >
           {showMenu ? <AiOutlineClose /> : <CgMenuLeft />}
         </button>
       )}
