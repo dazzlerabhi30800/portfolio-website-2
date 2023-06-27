e68945944ec5f4ae885bd71df90f61de97fabdf2
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-06-27 12:43:52 +0530

    added swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import { About } from "./Components/About";
 import { HeroSection } from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
 import { Projects } from "./Components/Projects";
-console.log("hello in arco linux");
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       <Navbar />
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,20 +1,56 @@
 import React from "react";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import "swiper/css";
+import "swiper/css/pagination";
+import { Resize } from "./Resize";
+
 
 export const Projects = () => {
+  const windowSize = Resize();
+  // console.log(windowSize);
   return (
     <section className="project--section" id="projects">
-      <h2>
+      <h2 data-aos="fade-up">
         Projects <span className="shadow--font">Projects</span>
       </h2>
-      <div className="projects--wrapper">
-        <div className="project--card">
+      <div className="project--wrapper">
+      <Swiper
+      spaceBetween={20}
+      slidesPerView={windowSize < 600 ? 1 : 2}
+      className="mySwiper"
+      rewind={true}
+      >
+        <SwiperSlide>
+        <div className="project--card" >
           <div className="card">
-            <img src="./Project-img/rock-paper-scissor.png" alt="" />
+            <img src="./Project-img/epic-games.png" alt="" />
             <h3>Rock Paper Scissors</h3>
             <button>Github</button>
             <button>Live Url</button>
           </div>
         </div>
+        </SwiperSlide>
+<SwiperSlide >
+        <div className="project--card" > 
+          <div className="card">
+            <img src="./Project-img/movie-app.png" alt="" />
+            <h3>Rock Paper Scissors</h3>
+            <button>Github</button>
+            <button>Live Url</button>
+          </div>
+        </div>
+        </SwiperSlide>
+<SwiperSlide >
+        <div className="project--card" > 
+          <div className="card">
+            <img src="./Project-img/netflix-landing-page.png" alt="" />
+            <h3>Rock Paper Scissors</h3>
+            <button>Github</button>
+            <button>Live Url</button>
+          </div>
+        </div>
+        </SwiperSlide>
+      </Swiper>
       </div>
     </section>
   );
