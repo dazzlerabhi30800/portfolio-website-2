b57e9aea874107f7534032b63582700c6aa20e85
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-06-27 13:05:48 +0530

    added projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,56 +1,46 @@
 import React from "react";
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Resize } from "./Resize";
-
+import ProjectData from "./Styles/data/ProjectData.json";
 
 export const Projects = () => {
   const windowSize = Resize();
-  // console.log(windowSize);
+  const pData = ProjectData.projectData;
   return (
     <section className="project--section" id="projects">
       <h2 data-aos="fade-up">
         Projects <span className="shadow--font">Projects</span>
       </h2>
       <div className="project--wrapper">
-      <Swiper
-      spaceBetween={20}
-      slidesPerView={windowSize < 600 ? 1 : 2}
-      className="mySwiper"
-      rewind={true}
-      >
-        <SwiperSlide>
-        <div className="project--card" >
-          <div className="card">
-            <img src="./Project-img/epic-games.png" alt="" />
-            <h3>Rock Paper Scissors</h3>
-            <button>Github</button>
-            <button>Live Url</button>
-          </div>
-        </div>
-        </SwiperSlide>
-<SwiperSlide >
-        <div className="project--card" > 
-          <div className="card">
-            <img src="./Project-img/movie-app.png" alt="" />
-            <h3>Rock Paper Scissors</h3>
-            <button>Github</button>
-            <button>Live Url</button>
-          </div>
-        </div>
-        </SwiperSlide>
-<SwiperSlide >
-        <div className="project--card" > 
-          <div className="card">
-            <img src="./Project-img/netflix-landing-page.png" alt="" />
-            <h3>Rock Paper Scissors</h3>
-            <button>Github</button>
-            <button>Live Url</button>
-          </div>
-        </div>
-        </SwiperSlide>
-      </Swiper>
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={windowSize < 600 ? 1 : 2}
+          className="mySwiper"
+          rewind={true}
+        >
+          {pData.map((data, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <div className="project--card">
+                  <div className="card">
+                    <img src={data.image} alt={data.name} />
+                    <h3>{data.name}</h3>
+                    <div className="link--wrapper">
+                      <a target="_blank" href={data.github}>
+                        Github
+                      </a>
+                      <a target="_blank" href={data.live}>
+                        Live Url
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </section>
   );
