f255ebc911bff1bc6504ba2e95316000b1655951
= <abhisingh30800@gmail.com>
2023-11-25 15:18:45 +0530

    added nav buttons in project swiper

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,34 +1,52 @@
+import { useRef, useState } from "react";
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Resize } from "./Resize";
 import ProjectData from "./Styles/data/ProjectData.json";
+import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 
 export const Projects = () => {
   const windowSize = Resize();
   const pData = ProjectData.projectData;
+  const swiperRef = useRef();
+  const [end, setEnd] = useState(false);
+  const [begin, setBegin] = useState(true);
+
   return (
     <section className="project--section" id="projects">
       <h2 data-aos="fade-up">
         Projects <span className="shadow--font">Projects</span>
       </h2>
+      <div className="navigation--buttons">
+        <button disabled={begin} onClick={() => swiperRef.current?.slidePrev()} className="nav--btn"><IoChevronBack /></button>
+        <button disabled={end} onClick={() => swiperRef.current?.slideNext()} className="nav--btn"><IoChevronForward /></button>
+      </div>
       <div className="project--wrapper">
         <Swiper
           modules={[Pagination]}
           pagination={{ clickable: windowSize > 600 && true }}
           spaceBetween={windowSize > 1400 ? 50 : 18}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          onSlideChange={(swiper) => {
+            setEnd(swiper.isEnd);
+            setBegin(swiper.isBeginning);
+          }}
           slidesPerView={
             windowSize < 600
               ? 1.1
               : windowSize > 1000
-              ? windowSize > 1200
-                ? windowSize > 1400
-                  ? 3
-                  : 2.8
-                : 2
-              : 1.8
+                ? windowSize > 1200
+                  ? windowSize > 1400
+                    ? 3
+                    : 2.8
+                  : 2
+                : 1.8
           }
+          // loop={true}
           className="mySwiper"
         >
           {pData.map((data, index) => {
