bd9af0b4cf27ac4cee6da6692597a517d2a53392
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-01-20 12:24:44 +0530

    added lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { About } from "./Components/About";
 import { HeroSection } from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
@@ -15,13 +15,39 @@ import FollowingDot from "./Components/FollowingDot";
 import { Resize } from "./Components/Resize";
 import Certifications from "./Components/Certfications";
 import SocialLinks from "./Components/SocialLinks";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
   const [show, setShow] = useState(false);
   const windowSize = Resize();
+  const lenis = useRef(null);
 
   useEffect(() => {
-    AOS.init();
+    // Initialize Lenis
+    lenis.current = new Lenis({
+      duration: 0.6, // Control the duration of the scroll
+      easing: (t) => 1 - Math.pow(1 - t, 3), // Cubic easing for smooth stop
+      smooth: true,
+      smoothTouch: true, // Enable smooth scrolling on touch devices
+    });
+
+    const animate = (time) => {
+      lenis.current.raf(time);
+      requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate);
+
+    // Cleanup on unmount
+    return () => {
+      lenis.current.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
     if (document.fonts && document.fonts.ready) {
       document.fonts.ready.then((res) => {
         setTimeout(() => {
