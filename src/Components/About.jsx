7530352cd6c92a8b4a0b9f5450304f330b2b1e20
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-03-26 10:18:06 +0530

    updated about bio #2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,10 +11,11 @@ export const About = () => {
       </h2>
       <p className="about--info">
         I'm a Frontend Developer with hands-on experience of 3 years, mainly
-        working on React & Next.js, while learning I've built numerous projects
-        that are operational with live deployment. Up until now, I've gathered
-        enough knowledge, that I can feel confident in myself & and can solve
-        any problem, no matter how complex or challenging.
+        working on React and Next.js, while learning I've built numerous
+        projects that are operational with live deployment. Up until now, I've
+        gathered enough knowledge, that I can feel confident in myself and has
+        the ability to tackle any challenge, no matter how complex or
+        challenging.
       </p>
     </section>
   );
